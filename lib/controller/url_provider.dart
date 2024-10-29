09306d16cc88f726312234f09cb59ae9829f9280
Prashant_Gaikwad_701 <165748489+Pbg701@users.noreply.github.com>
2024-10-29 12:26:40 +0530

    codes

---
+++ lib/controller/url_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:web/models/url_model.dart';
+
+class UrlProvider with ChangeNotifier {
+  UrlModel _currentUrl = UrlModel('https://docs.flutter.dev/packages-and-plugins/using-packages');
+
+  String get currentUrl => _currentUrl.url;
+
+  void updateUrl(String newUrl) {
+    _currentUrl = UrlModel(newUrl);
+    notifyListeners();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:web/controller/url_provider.dart';
+import 'package:web/presentation/screens/home_screen.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UrlProvider()),
+      ],
+      child: MaterialApp(
+        title: 'WebView Gaming Platform',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
